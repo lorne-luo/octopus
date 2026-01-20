8a45ba2654bc5d7575f33e5cbb5ad08807552a7a
Lorne <dev@luotao.net>
2026-01-20 23:30:27 +1100

    chore: update upgrade script for build and docker
    Updates the upgrade script to build the application for linux arm64, copy the binary to the docker directory, and then restart the docker compose services.

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+rm -rf build
+bash scripts/build.sh build linux arm64
+mkdir -p build/docker/linux/arm64/
+mv build/bin/octopus-linux-arm64 build/docker/linux/arm64/octopus
 docker compose down
 docker rmi lorne/octopus
 docker compose up -d
