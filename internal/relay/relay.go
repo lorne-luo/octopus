e0bfacc18422dc09e632e1f4f34933882f7dff04
Lorne <dev@luotao.net>
2026-02-04 13:13:17 +1100

    fix(relay): handle context canceled error

--- internal/relay/relay.go
+++ internal/relay/relay.go
@@ -433,12 +433,18 @@ func (ra *relayAttempt) transformStreamData(ctx context.Context, data string) ([
 func (ra *relayAttempt) handleResponse(ctx context.Context, response *http.Response) error {
 	internalResponse, err := ra.outAdapter.TransformResponse(ctx, response)
 	if err != nil {
+		if ctx.Err() == context.Canceled {
+			return fmt.Errorf("request canceled by client: %w", context.Canceled)
+		}
 		log.Warnf("failed to transform response: %v", err)
 		return fmt.Errorf("failed to transform outbound response: %w", err)
 	}
 
 	inResponse, err := ra.inAdapter.TransformResponse(ctx, internalResponse)
 	if err != nil {
+		if ctx.Err() == context.Canceled {
+			return fmt.Errorf("request canceled by client: %w", context.Canceled)
+		}
 		log.Warnf("failed to transform response: %v", err)
 		return fmt.Errorf("failed to transform inbound response: %w", err)
 	}
